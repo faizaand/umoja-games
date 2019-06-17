ffbb3f1f4543381f784cf2b5880c8ae9fd70036b
Faizaan Datoo <fdatoo7@gmail.com>
2019-06-17 00:20:16 -0400

    New concept for homepage design

---
+++ src/app/components/schedule-event/schedule-event.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-event',
+  templateUrl: './schedule-event.component.html',
+  styleUrls: ['./schedule-event.component.scss'],
+})
+export class ScheduleEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
