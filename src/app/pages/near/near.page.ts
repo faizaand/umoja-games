627595d7229cb979dc486d45938b6edf88992d10
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-13 00:30:18 -0400

    Scaffolding and home page

---
+++ src/app/pages/near/near.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-near',
+  templateUrl: './near.page.html',
+  styleUrls: ['./near.page.scss'],
+})
+export class NearPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
