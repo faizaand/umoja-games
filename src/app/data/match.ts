8a747e438ab85cc56e549d9db4f5a25870272d46
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-17 21:27:29 -0400

    refactor: go back to dummy data for now, TBC

---
+++ src/app/data/match.ts
@@ -0,0 +1,7 @@
+
+export interface Match {
+    team1Id: number;
+    team2Id: number;
+    score1: number;
+    score2: number;
+}
---
+++ src/app/data/player.ts
@@ -0,0 +1,7 @@
+
+export interface Player {
+    name: string;
+    nationality: string;
+    imageUrl: string;
+
+}
