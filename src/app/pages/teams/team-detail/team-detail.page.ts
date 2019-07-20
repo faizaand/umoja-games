4d09c5a187865af3103d2d9fc7c713bce35e751a
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-20 02:58:56 -0400

    feature: finally loading players and teams

--- src/app/pages/teams/team-detail/team-detail.page.ts
+++ src/app/pages/teams/team-detail/team-detail.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Team} from '../../../data/team';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {ActivatedRoute} from '@angular/router';
+import {Player} from '../../../data/player';
 
 @Component({
-  selector: 'app-team-detail',
-  templateUrl: './team-detail.page.html',
-  styleUrls: ['./team-detail.page.scss'],
+    selector: 'app-team-detail',
+    templateUrl: './team-detail.page.html',
+    styleUrls: ['./team-detail.page.scss'],
 })
 export class TeamDetailPage implements OnInit {
 
-  constructor() { }
+    team: Team = new class implements Team {
+        captain: string;
+        category: string;
+        name: string;
+    };
+    players: Player[];
+    selectedScreen: string = "players";
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {
+    }
+
+    ngOnInit() {
+        const doc = this.firestore.doc('teams/' + this.route.snapshot.params.id).valueChanges();
+        const playerCol = this.firestore.collection('teams/' + this.route.snapshot.params.id + '/players').valueChanges();
+        doc.subscribe(value => {
+            this.team = value as Team;
+        });
+
+        playerCol.subscribe(value => {
+            this.players = value as Player[];
+        });
+    }
 
 }
