2cb696c3a40069c558ab66a6ce60e5769d44026f
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-20 02:06:26 -0400

    dev: use Firestore for data

--- src/app/pages/matches/matches.page.ts
+++ src/app/pages/matches/matches.page.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatchService} from '../../match.service';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {Match} from '../../data/match';
 
 @Component({
-  selector: 'app-matches',
-  templateUrl: './matches.page.html',
-  styleUrls: ['./matches.page.scss'],
+    selector: 'app-matches',
+    templateUrl: './matches.page.html',
+    styleUrls: ['./matches.page.scss'],
 })
 export class MatchesPage implements OnInit {
 
-  categories: string[];
-  selectedCategory: string;
-  matchesCollection;
-  matches;
+    categories: string[];
+    selectedCategory: string;
+    matchesCollection;
+    matches;
 
-  constructor(private matchService: MatchService, private firestore: AngularFirestore) { }
+    constructor(private matchService: MatchService, private firestore: AngularFirestore) {
+    }
 
-  ngOnInit() {
-    this.categories = this.matchService.getCategories();
-    this.selectedCategory = this.categories[0];
-    this.matchesCollection = this.firestore.collection('matches');
-    this.matches = this.matchesCollection.valueChanges();
-  }
+    ngOnInit() {
+        this.categories = this.matchService.getCategories();
+        this.selectedCategory = this.categories[0];
+        this.fetchMatches();
+    }
 
-  segmentChanged($event: CustomEvent<any>) {
-    const selectedIndex = $event.detail.value.substr(-1, 1);
-    this.selectedCategory = this.categories[selectedIndex];
-  }
+    segmentChanged($event: CustomEvent<any>) {
+        const selectedIndex = $event.detail.value.substr(-1, 1);
+        this.selectedCategory = this.categories[selectedIndex];
+        this.fetchMatches();
+    }
+
+    fetchMatches() {
+        this.matchesCollection = this.firestore.collection('matches', ref => ref.where('category', '==', this.selectedCategory));
+        this.matchesCollection.snapshotChanges().subscribe(actions => {
+            this.matches = actions.map(a => {
+                const data = a.payload.doc.data() as Match;
+                const id = a.payload.doc.id;
+                return { id, ...data };
+            });
+        });
+    }
 }
