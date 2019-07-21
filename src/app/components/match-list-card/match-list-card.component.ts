bc9d9ce7e128863cdf76cab1fe19d81c53e2388a
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-21 01:03:00 -0400

    feature: update match card list UI

--- src/app/components/match-list-card/match-list-card.component.ts
+++ src/app/components/match-list-card/match-list-card.component.ts
@@ -1,17 +1,72 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Match} from '../../data/match';
 
 @Component({
-  selector: 'app-match-list-card',
-  templateUrl: './match-list-card.component.html',
-  styleUrls: ['./match-list-card.component.scss'],
+    selector: 'app-match-list-card',
+    templateUrl: './match-list-card.component.html',
+    styleUrls: ['./match-list-card.component.scss'],
 
 })
 export class MatchListCardComponent implements OnInit {
 
-  @Input() public match;
+    @Input() public match;
+    team1: any = {title: '', subtitle: ''};
+    team2: any = {title: '', subtitle: ''};
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+        this.team1 = this.computeTeamTitles(this.match.team1);
+        this.team2 = this.computeTeamTitles(this.match.team2);
+        this.computeTimeString(this.match.date, this.match.endDate);
+    }
+
+    computeTeamTitles(team: string) {
+        const teamSplit = team.split(' ');
+        let title = '';
+        let subtitle = '';
+        if (teamSplit.length === 1) {
+            title = teamSplit[0];
+            subtitle = '';
+        } else if (teamSplit.length === 2) {
+            subtitle = teamSplit[0];
+            title = teamSplit[1];
+        } else {
+            const part1 = teamSplit.slice(0, teamSplit.length - 2);
+            const part2 = teamSplit.slice(teamSplit.length - 2);
+
+            if (part2[1].length < 3) {
+                // the last part of the name is under 3 letters so it'll probably fit
+                subtitle = part1.join(' ');
+                title = part2.join(' ');
+            } else {
+                subtitle = part1.join(' ') + ' ' + part2[0];
+                title = part2[1];
+            }
+        }
+
+        return {
+            title, subtitle
+        };
+    }
+
+    computeTimeString(matchStartTimestamp, matchEndTimestamp) {
+        const matchStart = new Date(matchStartTimestamp.seconds * 1000);
+        const matchEnd = matchEndTimestamp == null ? null : new Date(matchEndTimestamp * 1000);
+        const now = new Date(2019, 7, 3, 15, 0, 0, 0);
+        console.log(matchStart);
+        console.log(matchEnd);
+        console.log(now);
+
+        /*
+        if(beforeTheMatch) {
+            if(today) {
+                // starting at 3:30pm
+            } else {
+                // Sunday 3:30pm
+            }
+        }*/
+    }
 
 }
