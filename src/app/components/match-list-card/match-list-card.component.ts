3a9e6a24cda5561415e6a2038f004b29635f23ba
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-21 23:33:18 -0400

    refactor: helper class and small ui changes

--- src/app/components/match-list-card/match-list-card.component.ts
+++ src/app/components/match-list-card/match-list-card.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Match} from '../../data/match';
+import {computeTeamTitles, computeTimeString} from '../../match-helper';
 
 @Component({
     selector: 'app-match-list-card',
@@ -17,56 +17,9 @@ export class MatchListCardComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.team1 = this.computeTeamTitles(this.match.team1);
-        this.team2 = this.computeTeamTitles(this.match.team2);
-        this.computeTimeString(this.match.date, this.match.endDate);
-    }
-
-    computeTeamTitles(team: string) {
-        const teamSplit = team.split(' ');
-        let title = '';
-        let subtitle = '';
-        if (teamSplit.length === 1) {
-            title = teamSplit[0];
-            subtitle = '';
-        } else if (teamSplit.length === 2) {
-            subtitle = teamSplit[0];
-            title = teamSplit[1];
-        } else {
-            const part1 = teamSplit.slice(0, teamSplit.length - 2);
-            const part2 = teamSplit.slice(teamSplit.length - 2);
-
-            if (part2[1].length < 3) {
-                // the last part of the name is under 3 letters so it'll probably fit
-                subtitle = part1.join(' ');
-                title = part2.join(' ');
-            } else {
-                subtitle = part1.join(' ') + ' ' + part2[0];
-                title = part2[1];
-            }
-        }
-
-        return {
-            title, subtitle
-        };
-    }
-
-    computeTimeString(matchStartTimestamp, matchEndTimestamp) {
-        const matchStart = new Date(matchStartTimestamp.seconds * 1000);
-        const matchEnd = matchEndTimestamp == null ? null : new Date(matchEndTimestamp * 1000);
-        const now = new Date(2019, 7, 3, 15, 0, 0, 0);
-        console.log(matchStart);
-        console.log(matchEnd);
-        console.log(now);
-
-        /*
-        if(beforeTheMatch) {
-            if(today) {
-                // starting at 3:30pm
-            } else {
-                // Sunday 3:30pm
-            }
-        }*/
+        this.team1 = computeTeamTitles(this.match.team1);
+        this.team2 = computeTeamTitles(this.match.team2);
+        computeTimeString(this.match.date, this.match.endDate);
     }
 
 }
--- src/app/pages/matches/match-detail/match-detail.page.ts
+++ src/app/pages/matches/match-detail/match-detail.page.ts
@@ -3,8 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {MatchService} from '../../../match.service';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Match} from '../../../data/match';
-import Timestamp = firebase.firestore.Timestamp;
-import * as firebase from 'firebase';
+import {computeTeamTitles, computeTimeString} from '../../../match-helper';
 
 @Component({
     selector: 'app-match-detail',
@@ -26,62 +25,15 @@ export class MatchDetailPage implements OnInit {
         const matchDocument = this.firestore.doc('matches/' + id).valueChanges();
         matchDocument.subscribe(value => {
             this.match = value as Match;
-            this.team1 = this.computeTeamTitles(this.match.team1);
-            this.team2 = this.computeTeamTitles(this.match.team2);
-            this.computeTimeString(this.match.date, this.match.endDate);
+            this.team1 = computeTeamTitles(this.match.team1);
+            this.team2 = computeTeamTitles(this.match.team2);
+            computeTimeString(this.match.date, this.match.endDate);
         });
     }
 
     segmentChanged($event) {
         const segmentId = Number($event.detail.value.substr(-1, 1));
         this.segment = segmentId ? 'rosters' : 'activity';
-    }
-
-    computeTeamTitles(team: string) {
-        const teamSplit = team.split(' ');
-        let title = '';
-        let subtitle = '';
-        if (teamSplit.length === 1) {
-            title = teamSplit[0];
-            subtitle = '';
-        } else if (teamSplit.length === 2) {
-            subtitle = teamSplit[0];
-            title = teamSplit[1];
-        } else {
-            const part1 = teamSplit.slice(0, teamSplit.length - 2);
-            const part2 = teamSplit.slice(teamSplit.length - 2);
-
-            if (part2[1].length < 3) {
-                // the last part of the name is under 3 letters so it'll probably fit
-                subtitle = part1.join(' ');
-                title = part2.join(' ');
-            } else {
-                subtitle = part1.join(' ') + ' ' + part2[0];
-                title = part2[1];
-            }
-        }
-
-        return {
-            title, subtitle
-        };
-    }
-
-    computeTimeString(matchStartTimestamp, matchEndTimestamp) {
-        const matchStart = new Date(matchStartTimestamp.seconds * 1000);
-        const matchEnd = matchEndTimestamp == null ? null : new Date(matchEndTimestamp * 1000);
-        const now = new Date(2019, 7, 3, 15, 0, 0, 0);
-        console.log(matchStart);
-        console.log(matchEnd);
-        console.log(now);
-
-        /*
-        if(beforeTheMatch) {
-            if(today) {
-                // starting at 3:30pm
-            } else {
-                // Sunday 3:30pm
-            }
-        }*/
     }
 
 }
