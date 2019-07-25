1874966e9254c7a691780032f92b2c85c3d3b033
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-25 00:25:36 -0400

    feature: player editing page

--- src/app/pages/admin/admin-team-edit/admin-team-edit.page.ts
+++ src/app/pages/admin/admin-team-edit/admin-team-edit.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Team} from '../../../data/team';
+import {Player} from '../../../data/player';
+import {DataService} from '../../../data/data.service';
+import {categories} from '../../../data/categories';
 
 @Component({
   selector: 'app-admin-team-edit',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminTeamEditPage implements OnInit {
 
-  constructor() { }
+  categories = categories.map(value => value.name);
+  selectedCategory: string;
+
+  teams = [];
+  selectedTeam: Team;
+
+  players = [];
+  selectedPlayer: Player;
+
+  constructor(private db: DataService) { }
 
   ngOnInit() {
+  }
+
+  onCategorySelect() {
+    this.db.getTeamsByCategory(this.selectedCategory).subscribe(value => {
+      this.teams = value;
+    });
+  }
+
+  onTeamSelect() {
+    this.db.getPlayersByTeam$(this.selectedTeam.id).subscribe(value => {
+      this.players = value;
+    });
+  }
+
+  onPlayerSelect() {
+
+  }
+
+  step(field: string, value: any) {
+    if(this.selectedPlayer[field] + value < 0) return;
+    this.selectedPlayer[field] = this.selectedPlayer[field] + value;
+    this.db.updatePlayer(this.selectedPlayer);
   }
 
 }
