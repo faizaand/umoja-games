8e299c2f77b127961ebd6ccc14bd6b1f6682f18f
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-26 13:30:55 -0400

    feature: no notifs, followed teams use local storage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Team} from '../../data/team';
+import {DataService} from '../../data/data.service';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+import {Storage} from '@ionic/storage';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+    followedTeams: Team[] = [];
+    links: any[] = [{title: '', description: '', url: ''}];
+    date = new Date();
 
-  ngOnInit() {
-  }
+    constructor(private db: DataService, private browser: InAppBrowser) {
+    }
+
+    ngOnInit() {
+        this.db.getLinks$().subscribe(links => {
+            this.links = links;
+        });
+    }
+
+    ionViewWillEnter() {
+        this.db.getFollowedTeams().then(teams => {
+            this.followedTeams = [];
+            teams.forEach(team => {
+                this.db.getTeamById(team).then(teamData => {
+                    const teamObj = teamData.data() as Team;
+                    this.followedTeams.push(teamObj);
+                });
+            });
+        });
+    }
+
+    link(url) {
+        this.browser.create(url);
+    }
 
 }
