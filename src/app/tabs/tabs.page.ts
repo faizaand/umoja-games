e0da9ba1c9210e091932957a5546f6dfb3b8aa22
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-26 17:58:04 -0400

    feature: password protect the umoji only center

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html',
+    styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
 
-  constructor() {}
+    umoji: boolean = false;
 
+    constructor(private storage: Storage) {
+    }
+
+    ngOnInit() {
+        this.storage.get('umoji').then(value => {
+            this.umoji = value;
+        });
+    }
 }
