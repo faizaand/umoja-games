d862e523a96050b333c1e98e9acc8b7dbc8fd772
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-28 01:49:14 -0400

    ios: more ios fixes

---
+++ src/environments/environmen.prod.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyC09IBhu5Weyw3LxW1YtajritwtseNJcb0",
+      authDomain: "umoja-games-ab076.firebaseapp.com",
+      databaseURL: "https://umoja-games-ab076.firebaseio.com",
+      projectId: "umoja-games-ab076",
+      storageBucket: "umoja-games-ab076.appspot.com",
+      messagingSenderId: "823149727939",
+      appId: "1:823149727939:web:242d4eee00975fc5"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
