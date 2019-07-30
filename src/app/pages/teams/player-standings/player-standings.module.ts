d3297e00c2bf4b9b29c958f9241889aa327dd13f
Faizaan Datoo <fdatoo7@gmail.com>
2019-07-30 02:04:25 -0400

    forgot to include this code

---
+++ src/app/pages/teams/player-standings/player-standings.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PlayerStandingsPage } from './player-standings.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PlayerStandingsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PlayerStandingsPage]
+})
+export class PlayerStandingsPageModule {}
